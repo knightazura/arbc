35f616b96efd650f9bca133844389d6cce2d0894
Muhammad Izzuddin al Fikri <knightazura@users.noreply.github.com>
2016-03-20 10:02:03 +0700

    Update and rename index.html to index.php
    Add meta tag for theme-color

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#484e58" />
     <title>ASEAN Reusable Bag Campaign</title>
     <link rel="stylesheet" href="css/foundation.min.css" />
     <link rel="stylesheet" href="css/fonts/foundation-icons.css" />
