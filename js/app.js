0fd7b3de0db790dab0a8913ba1bdae6640c937c2
Mizuki <azura.azureus.ikhie@gmail.com>
2016-04-07 23:41:53 +0700

    Latest layout

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 $(document).foundation();
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		menu: '#take-down',
-		anchors:['secondPage'],
-		autoScrolling: false,
-		scrollingSpeed: 1700,
-		scrollBar: true,
-		verticalCentered: false,
-		recordHistory: true
+	var orbit = new Foundation.Orbit($('#top-landing'), {
+		animation: 'fade',
+	    timerDelay: 10000,
+	    pauseOnHover: true,
+	    navButtons: false,
+	    bullets: false
 	});
 
+	/*
+	$('#fullpage').fullpage({
+			menu: '#take-down',
+			anchors:['secondPage'],
+			autoScrolling: false,
+			scrollingSpeed: 1700,
+			scrollBar: true,
+			verticalCentered: false,
+			recordHistory: true
+	});
+	
 	var wh = $(window).height(),
 		ww = $(window).width()
 	console.log('Browser height viewport : ' + wh);
+	*/
 });
+
 $.stellar({
 	horizontalScrolling: false,
 	hideDistantElements: false,
-	
 });
